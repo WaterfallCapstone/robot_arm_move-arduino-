93ea8fcf4f64e0ff29abc5e6e11435f9d63e82f5
HCHHEE <106053186+HCHHEE@users.noreply.github.com>
2023-05-09 09:52:33 +0900

    Update robot_arm_move.ino

--- robot_arm_move.ino
+++ robot_arm_move.ino
@@ -2,20 +2,20 @@
 #define I2CAdd 0x40
 HCPCA9685 HCPCA9685(I2CAdd); 
 String str = "";
-int angle[6] = [90, 90, 90, 90, 90, 90];
+int angle[6] = {90, 90, 90, 90, 90, 90};
 
 void angle_Set(String nstr)//get angle order from pyhton and save in angle[4]-> when this method activate arr angle is changed
 {
   int temp = 0;
   int index = nstr.indexOf(' ');
-  for(int i = 0; i < 5; i++){
+  for(int i = 0; i < 6; i++){
     angle[i] = nstr.substring(temp, index).toInt();
     temp = index + 1;
     index = nstr.indexOf(' ', temp);
   }
   for(int i=0;i<6;i++){
-    if(i==3||i==5) angle[i]=map(angle[i],0,180,420,0);
-    else angle[i]=map(angle[i],0,180,0,420);
+    if(i==3||i==5) angle[i]=map(angle[i],0,180,400,0);
+    else angle[i]=map(angle[i],0,180,0,400);
   }
 }
 
